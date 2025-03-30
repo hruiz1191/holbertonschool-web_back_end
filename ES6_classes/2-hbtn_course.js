18d8d36226944a233340b3adf37bdb235489ecd6
hruiz <elproferuiz@yahoo.com>
2025-03-30 16:47:58 -0400

    task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // Validación de tipos
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+// Getter y setter para name
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+    }
+// Getter y setter para length
+  get length() {
+    return this._length;
+  }
+  
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Getter y setter para students
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
