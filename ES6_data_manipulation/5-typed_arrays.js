f071cd0f52147b931c729620e0abe408583138c1
hruiz <elproferuiz@yahoo.com>
2025-04-03 16:10:59 -0400

    task 5 try 4

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,11 +2,12 @@ export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
-
-  const buffer = new ArrayBuffer(length); // crea la memoria
-  const view = new DataView(buffer);      // crea una vista manipulable
-
-  view.setInt8(position, value);          // coloca el valor en la posición
+  // crea la memoria
+  const buffer = new ArrayBuffer(length);
+  // crea una vista manipulable
+  const view = new DataView(buffer);
+  // coloca el valor en la posición
+  view.setInt8(position, value);
 
   return view;
 }
