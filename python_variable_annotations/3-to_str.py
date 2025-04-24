140cb02547e76c60fdf81a23bdf940b77808c9ed
hruiz <elproferuiz@yahoo.com>
2025-04-24 14:04:13 -0400

    task 3 y 5   try 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -4,6 +4,7 @@ Module 3-to_str
 This module provides a function to convert a float to a string.
 """
 
+
 def to_str(n: float) -> str:
     """Returns the string representation of a float."""
     return str(n)
--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,6 +5,7 @@ This module provides a function to sum a list of floats.
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats."""
     return sum(input_list)
