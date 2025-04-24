2267a595adfc7f96c336c48126e9355edf179593
hruiz <elproferuiz@yahoo.com>
2025-04-24 13:05:58 -0400

    try 11

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """
+Module 0-add
 This module provides a function to add two floating-point numbers.
 """
 
 
 def add(a: float, b: float) -> float:
+    """Add two floating-point numbers and return the result."""
     return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -6,4 +6,5 @@ This module provides a function to concatenate two strings.
 
 
 def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the result."""
     return str1 + str2
