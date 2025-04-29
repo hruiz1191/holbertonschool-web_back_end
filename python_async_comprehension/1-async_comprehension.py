fe3203cac5594d796cb269abe1c9f1559eab9ee9
hruiz <elproferuiz@yahoo.com>
2025-04-29 11:42:55 -0400

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-import asyncio
-import random
+""" File that contains the function async comprehension """
 
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension():
-    return [i async for i in async_generator()]
 
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random float numbers asynchronously using async_generator.
 
-    end_time = time.perf_counter()
-
-    total_time = end_time - start_time
-    return total_time
+    Returns:
+        List[float]: A list of 10 random floating-point numbers between 0 and 10.
+    """
+    return [number async for number in async_generator()]
