0e73611e293fa318110d496b9154059c85d53183
hruiz <elproferuiz@yahoo.com>
2025-05-01 20:51:31 -0400

    trask 2 try 8

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -7,7 +7,7 @@ from typing import List, Tuple, Dict
 
 def index_range(page: int, page_size: int) -> Tuple:
     """ Function that returns the index and size of a page """
-    return ((page * page_size) - page_size, page * page_size)
+    return (((page * page_size) - page_size), (page * page_size))
 
 
 class Server:
@@ -23,27 +23,31 @@ class Server:
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
-                self.__dataset = [row for row in reader][1:]  # Skip header
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ Displays the correct amount of items per page """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
-
         data = Server.dataset(self)
-        start, end = index_range(page, page_size)
-        return data[start:end]
+        pp = index_range(page, page_size)
+        return data[pp[0]:pp[1]]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """ Displays the page information """
         data = Server.dataset(self)
-        total_pages = len(data) // page_size + (1 if len(data) % page_size != 0 else 0)
+        total_pages = len(data) // page_size
+        next_page = None
+        if page < total_pages:
+            next_page = page + 1
+        prev_page = None
+        if page > 1:
+            prev_page = page - 1
 
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
-
-        return {
+        page_info = {
             "page_size": page_size,
             "page": page,
             "data": self.get_page(page, page_size),
@@ -51,4 +55,4 @@ class Server:
             "prev_page": prev_page,
             "total_pages": total_pages
         }
-
+        return page_info
