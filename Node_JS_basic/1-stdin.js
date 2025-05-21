866ee6dc2a38270757774ea831b46fd2934ebf15
hruiz <elproferuiz@yahoo.com>
2025-05-21 12:49:46 -0400

    tasl 1 try 3

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,10 +2,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${name}\r`);
 });
 
-// El mensaje de cierre solo se imprime cuando el proceso termina
 process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
