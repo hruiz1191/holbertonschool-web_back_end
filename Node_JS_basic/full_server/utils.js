12bf499cf526a63f5e77c87a5250d028aa28307f
hruiz <elproferuiz@yahoo.com>
2025-05-23 14:01:29 -0400

    task 8 try 9

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+import fs from 'fs';
 
-function readDatabase(path) {
-  return new Promise((resolve, reject) => {
+const readDatabase = (filePath) => {
+   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(Error(err));
