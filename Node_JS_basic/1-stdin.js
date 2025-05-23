38d096123cfbf3524ad997b172d73d9eb4a28470
hruiz <elproferuiz@yahoo.com>
2025-05-23 12:54:06 -0400

    task 1 try 15

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${name}`);
 });
 
 process.stdin.on('exit', () => {
