16911fa5d7beac457680d5f36d3f058b48a16815
hruiz <elproferuiz@yahoo.com>
2025-05-23 13:42:40 -0400

    task  8 try 6

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,11 +3,11 @@ import { readDatabase } from '../utils';
 export default class StudentsController {
   static async getAllStudents(req, res) {
     const database = process.argv[2] || 'database.csv';
-    
+
     try {
       const fields = await readDatabase(database);
       let response = 'This is the list of our students\n';
-      
+  
       Object.keys(fields).sort().forEach((field) => {
         response += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
       });
