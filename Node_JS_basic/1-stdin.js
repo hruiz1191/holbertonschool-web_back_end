b7059273c0b84776b61ab7a2b4665e657c149cb6
hruiz <elproferuiz@yahoo.com>
2025-05-23 13:04:29 -0400

    task 1 try 18

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,6 +7,6 @@ process.stdin.on('readable', () => {
   }
 });
 
-process.stdin.on('exit', () => {
+process.stdin.on('close', () => {
   process.stdout.write('This important software is now closing\n');
 });
