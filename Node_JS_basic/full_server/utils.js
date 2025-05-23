f1a81818bd2f672fb0d415aef96e9d80bb11c4d0
hruiz <elproferuiz@yahoo.com>
2025-05-23 13:09:47 -0400

    task 8  utils

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-export function readDatabase(filePath) {
+function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
@@ -22,3 +22,5 @@ export function readDatabase(filePath) {
     });
   });
 }
+
+export default readDatabase;
