751a9381e11a8dde8d3f3a1fd24ad0c90fa6850a
hruiz <elproferuiz@yahoo.com>
2025-05-23 11:22:49 -0400

    task 1 try 12

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,8 +2,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r\n`);  // Usamos \r\n para Windows
-  process.exit();  // Forzamos la salida después de escribir el nombre
+  process.stdout.write(`Your name is: ${name}\r\n`);
+  process.exit();
 });
 
 process.on('exit', () => {
